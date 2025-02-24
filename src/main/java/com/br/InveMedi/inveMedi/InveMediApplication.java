97d6ab7322ab72c731941d526ac21861273e8e48
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-02-24 18:49:05 -0300

    iniciando api - dependencias

---
+++ src/main/java/com/br/InveMedi/inveMedi/InveMediApplication.java
@@ -0,0 +1,13 @@
+package com.br.InveMedi.inveMedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InveMediApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InveMediApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/InveMedi/inveMedi/InveMediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.InveMedi.inveMedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InveMediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
