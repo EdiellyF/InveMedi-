585cca9c484b93b48aaf7535bb78e999c36f9a94
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-02-26 12:11:10 -0300

    merge: repositories

--- src/main/java/com/br/InveMedi/inveMedi/repositories/TipoItemHospitalar.java
+++
@@ -1,6 +0,0 @@
-package com.br.InveMedi.inveMedi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoItemHospitalar extends JpaRepository<TipoItemHospitalar, Long> {
-}
